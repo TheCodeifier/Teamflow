a4180a8c722d89e5c5d4010225ec64a814be4109
Sjoerdle <Lunshofsjoerd@gmail.com>
2025-04-10 11:54:24 +0200

    Message class aangemaakt met de getters

---
+++ src/Message.java
@@ -0,0 +1,51 @@
+import java.time.LocalDateTime;
+
+public class Message {
+    private int id;
+    private String content;
+    private User sender;
+    private LocalDateTime timestamp;
+    private String trelloUrl;
+    private int sprintnummer;
+
+    public Message (String content, User sender, LocalDateTime timestamp, String trelloUrl, int sprintnummer){
+        this.content = content;
+        this.sender = sender;
+        this.timestamp = LocalDateTime.now();
+        this.trelloUrl = trelloUrl;
+        this.sprintnummer = sprintnummer;
+    }
+
+    public Message (int id, String content, User sender, LocalDateTime timestamp, String trelloUrl, int sprintnummer){
+        this.id = id;
+        this.content = content;
+        this.sender = sender;
+        this.timestamp = timestamp;
+        this.trelloUrl = trelloUrl;
+        this.sprintnummer = sprintnummer;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public User getSender(){
+        return sender;
+    }
+
+    public LocalDateTime getTimestamp(){
+        return timestamp;
+    }
+
+    public String getTrelloUrl(){
+        return trelloUrl;
+    }
+
+    public int getSprintnummer(){
+        return sprintnummer;
+    }
+}
